20b01eceefa10c0ee25209d7d7f480229e823c0a
mdeniz0 <mehmetdeniz@mistelektronik.com>
2023-05-22 14:37:38 +0300

    RS422 test files updated

--- test_ch1_rs422_orin.sh
+++ test_ch1_rs422_orin.sh
@@ -6,12 +6,18 @@ fi
 
 sudo echo 474 > /sys/class/gpio/export
 sudo echo 473 > /sys/class/gpio/export
+sudo echo 472 > /sys/class/gpio/export
+sudo echo 469 > /sys/class/gpio/export
 
 sudo echo high > /sys/class/gpio/PY.04/direction
 sudo echo low > /sys/class/gpio/PY.03/direction
+sudo echo high > /sys/class/gpio/PY.02/direction
+sudo echo low > /sys/class/gpio/PX.07/direction
 
 sudo gtkterm -p /dev/ttyTHS0 -s 115200
 
 sudo echo 474 > /sys/class/gpio/unexport
 sudo echo 473 > /sys/class/gpio/unexport
+sudo echo 472 > /sys/class/gpio/unexport
+sudo echo 469 > /sys/class/gpio/unexport
 
