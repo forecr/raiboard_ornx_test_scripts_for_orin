c5573ec2e0067ea341c68725c0656230f6e97ae5
mdeniz0 <mehmetdeniz@mistelektronik.com>
2023-11-03 18:38:17 +0300

    M.2 Key-E & Key-B commands updated. Unnecessary commands closed. USB mux GPIO added for rev-1.1

--- test_key_b_orin.sh
+++ test_key_b_orin.sh
@@ -1,22 +1,25 @@
-sudo echo 454 > /sys/class/gpio/export
+#sudo echo 454 > /sys/class/gpio/export
 sudo echo 433 > /sys/class/gpio/export
-sudo echo 328 > /sys/class/gpio/export
+#sudo echo 328 > /sys/class/gpio/export
 sudo echo 331 > /sys/class/gpio/export
-sudo echo 446 > /sys/class/gpio/export
+#sudo echo 446 > /sys/class/gpio/export
+sudo echo 492 > /sys/class/gpio/export # USB_Mux
 
 #sudo echo low > /sys/class/gpio/PQ.06/direction
 sudo echo high > /sys/class/gpio/PN.01/direction
 #sudo echo high > /sys/class/gpio/PCC.00/direction
 sudo echo low > /sys/class/gpio/PCC.03/direction
 #sudo echo high > /sys/class/gpio/PP.06/direction
+sudo echo low > /sys/class/gpio/PAC.06/direction
 
 trap interrupt_func INT
 interrupt_func() {
-	sudo echo 454 > /sys/class/gpio/unexport
+	#sudo echo 454 > /sys/class/gpio/unexport
 	sudo echo 433 > /sys/class/gpio/unexport
-	sudo echo 328 > /sys/class/gpio/unexport
+	#sudo echo 328 > /sys/class/gpio/unexport
 	sudo echo 331 > /sys/class/gpio/unexport
-	sudo echo 446 > /sys/class/gpio/unexport
+	#sudo echo 446 > /sys/class/gpio/unexport
+	sudo echo 492 > /sys/class/gpio/unexport
 }
 
 watch -n 0.1 lsusb
--- test_key_e_orin.sh
+++ test_key_e_orin.sh
@@ -1,13 +1,16 @@
-sudo echo 460 > /sys/class/gpio/export
-sudo echo 391 > /sys/class/gpio/export
+#sudo echo 460 > /sys/class/gpio/export
+#sudo echo 391 > /sys/class/gpio/export
+sudo echo 492 > /sys/class/gpio/export # USB_Mux
 
 #sudo echo low > /sys/class/gpio/PR.04/direction
 #sudo echo low > /sys/class/gpio/PH.00/direction
+sudo echo high > /sys/class/gpio/PAC.06/direction
 
 trap interrupt_func INT
 interrupt_func() {
-	sudo echo 460 > /sys/class/gpio/unexport
-	sudo echo 391 > /sys/class/gpio/unexport
+	#sudo echo 460 > /sys/class/gpio/unexport
+	#sudo echo 391 > /sys/class/gpio/unexport
+	sudo echo 492 > /sys/class/gpio/unexport
 }
 
 watch -n 0.1 lsusb
