14fd2921bba3015846bb1949083c705851367e0b
mdeniz0 <mehmet@forecr.io>
2024-10-15 15:13:18 +0300

    JetPack-6 network speed test updated for different net names

--- JetPack-6/test_net_speed.sh
+++ JetPack-6/test_net_speed.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 echo "Available Networks:"
 ip -br address | grep UP
-echo ""
-echo "Eth0: "
-sudo ethtool eth0 | grep Speed
-echo ""
-echo "Eth1:"
-sudo ethtool eth1 | grep Speed
-echo ""
+read -p "Enter the network name: " net_name
+sudo ethtool $net_name | grep Speed
 read -p 'Press [Enter] to exit' quit_key
 
